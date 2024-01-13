f9e10d49de8db2457d00f3941c807d16f1303e8f
MD Nur Hossen Rome <137864853+nrromie@users.noreply.github.com>
2024-01-13 22:52:32 +0600

    Added Dashboard Publicaion Statistics

---
+++ src/pages/Dashboard/Analytics/Analytics.jsx
@@ -0,0 +1,39 @@
+import { Chart } from "react-google-charts";
+import useAxiosPublic from "../../../hooks/useAxiosPublic";
+import Loading from "../../../components/Loading/Loading";
+import { useQuery } from "@tanstack/react-query";
+
+const Analytics = () => {
+
+    const axiosPublic = useAxiosPublic();
+    const { isPending, error, data: pubStats } = useQuery({
+        queryKey: ['repoData'],
+        queryFn: () =>
+            axiosPublic.get('/publication-stats').then((res) => res.data),
+    });
+
+    if (isPending) return <Loading />
+
+    if (error) { console.error(error) }
+
+    const data = [
+        ["Publication", "Number of Articles"],
+        ...pubStats.map(({ publication, articleCount }) => [publication, articleCount]),
+    ];
+
+    const options = {
+        title: "Articles of publishers",
+    };
+
+    return (
+        <Chart
+            chartType="PieChart"
+            data={data}
+            options={options}
+            width={"100%"}
+            height={"400px"}
+        />
+    );
+};
+
+export default Analytics;
